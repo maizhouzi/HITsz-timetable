41df7b7c9376653551c90132e953b74d3ca6cdf6
maizhouzi <maizhouzi@qq.com>
2021-05-04 17:32:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,48 +9,44 @@ import misc
 import datetime
 # import genIcs
 
+
 def main():
     form_url, form_inputs = excelCrawler.get_text()
     excelCrawler.login(form_url, form_inputs)
     excelRawBytes = excelCrawler.getExcelRawData()
 
     cal_name, cal_data = ProcessExcel.process(excelRawBytes)
+    cal_name = "你的课表"
     icalOutput.output(cal_name, cal_data)
 
 
 @click.command()
+@click.option('--username', '-u', help="Username")
+@click.option('--password',
+              '-p',
+              help="Password",
+              hide_input=True,
+              prompt="Enter Password")
 @click.option(
-    '--username', '-u',
-    help="Username")
-@click.option(
-    '--password', '-p',
-    help="Password",
-    hide_input=True,
-    prompt="Enter Password")
-@click.option(
-    '--filepath', '-f',
-    help="ics file path to be written. If not specified, contents will be written to stdout")
-@click.option(
-    '-y',
-    type=int,
-    help="Date of 1st Monday of the 1st week: year")
-@click.option(
-    '-m',
-    type=int,
-    help="Date of 1st Monday of the 1st week: month")
-@click.option(
-    '-d',
-    type=int,
-    help="Date of 1st Monday of the 1st week: day")
+    '--filepath',
+    '-f',
+    help=
+    "ics file path to be written. If not specified, contents will be written to stdout"
+)
+@click.option('-y', type=int, help="Date of 1st Monday of the 1st week: year")
+@click.option('-m', type=int, help="Date of 1st Monday of the 1st week: month")
+@click.option('-d', type=int, help="Date of 1st Monday of the 1st week: day")
 @click.option(
     '--stdout',
-    help="Write iCalendar file content to stdout, this will override --filepath option",
+    help=
+    "Write iCalendar file content to stdout, this will override --filepath option",
     is_flag=True)
 @staticmethod
 def set_vpn_port(port):
     socks5 = f"socks5h://127.0.0.1:{port}"
     proxies = {"http": socks5, "https": socks5}
     return proxies
+
 
 def execute(username, password, filepath, y, m, d, stdout):
     cmdInput.parseLoginParams(username, password)
@@ -59,6 +55,5 @@ def execute(username, password, filepath, y, m, d, stdout):
     main()
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     execute()
