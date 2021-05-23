344567213506fa4f69b12da93b5db93f777e0f07
maizhouzi <maizhouzi@qq.com>
2021-05-24 00:37:27 +0800

    update

--- crawler/excelCrawler.py
+++ crawler/excelCrawler.py
@@ -10,94 +10,138 @@ import requests
 import bs4
 import json
 import misc
-
-# Initialize a requests.Session with a fake header
-sess = requests.Session()
-sess.headers.update({
-    "Connection": "keep-alive",
-    "Cache-Control": "max-age=0",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.9"
-})
-
-socks5 = f"socks5h://127.0.0.1:1080"
-proxies1 = {"http": socks5, "https": socks5}
+import logging
 
 
-# GET login page, in order to:
-# - generates all input param of the login form
-# - get the login form action page
-# 
-# returns:
-# - URL of form action page
-# - ALL website default input key-values
-def get_text() -> typing.Tuple[str, dict]:
-    response = sess.get(config.URLs.login_page,proxies=proxies1)
-    if response.status_code != 200:
-        raise CrawlerError("Get Login Page: Server responded error code" + str(response.status_code) + ".")
+class ReportException(Exception):
+    """上报异常错误信息"""
+    class LoginError(Exception):
+        """登录失败"""
 
-    pageSoup = bs4.BeautifulSoup(response.text, "html.parser")
-    formSoup = pageSoup.find("form")
+    class SubmitError(Exception):
+        """上报失败"""
 
-    # Get form's attribute "action", which means action page
-    form_page = formSoup.get("action")
-
-    # Get all form input key and default values
-    default_values = dict()
-    inputSoups = formSoup.find_all("input")
-    for i in inputSoups:
-        default_values[i.get("name")] = i.get("value")
-
-    return form_page, default_values
+    class ReportExistError(Exception):
+        """已经上报"""
 
 
-# log in jw system
-def login(loginFormPage, allLoginParams):
-    # Add the given username and password into login params.
-    allLoginParams["username"] = config.CrawlerParams.username
-    allLoginParams["password"] = config.CrawlerParams.password
+class Report(object):
+    def __init__(self, proxy_on=True, ports=None):
+        """参数初始化"""
 
-    # Login!
-    # Get a validated COOKIE for our session.
-    response = sess.post(config.URLs.login_domain + loginFormPage, params=allLoginParams,proxies=proxies1)
+        self.proxy_on = proxy_on
+        self.proxy_ports = [] if ports is None else ports
 
-    if response.status_code != 200:
-        raise CrawlerError("Login: Server responded error code: " + str(response.status_code) + ".")
-    elif response.text.find("账号密码验证失败")!=-1:
-        raise CrawlerError("Login: Incorrect username or password.")
+        self.proxies = self.config_proxies()
+        self.session = requests.session()
 
+    def start_new_session(self):
+        sess = requests.session()
+        sess.headers.update({
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'
+        })
+        self.session = sess
 
-def getExcelRawData() -> bytes:
-    response = sess.post(config.URLs.uid_query,proxies=proxies1)
-    if response.status_code!=200:
-        raise CrawlerError("Query User UID: Server responded error code" + str(response.status_code) + ".")
-    try:
-        j = json.loads(response.text)
-        UID = j["ID"]
-    except json.JSONDecodeError:
-        raise CrawlerError("Query User UID: Get userinfo json from session failed.")
-    except KeyError:
-        raise CrawlerError("Query User UID: Cannot find id in requested userinfo json.")
-    
-    excel_params = {
-        "format": "excel",
-        "_filename_": "export"
-    }
+    def config_proxies(self, port: int = None):
+        if port and self.proxy_on:
+            socks5 = f"socks5h://127.0.0.1:{port}"
+            proxies = {"http": socks5, "https": socks5}
+            return proxies
+        else:
+            return None
 
-    year, smcount = misc.semester(config.DateTime.startDate)
-    # param "reportlets" seems to be a json-like stuff.
-    # inelegant, indeed. But I'm too lazy to make a change. ;)
-    excel_params["reportlets"] = """%5B%7B%22reportlet%22%3A%22%2Fbyyt%2Fpkgl%2F%E5%AD%A6%E7%94%9F%\
-E4%B8%BB%E9%A1%B5%E8%AF%BE%E8%A1%A8%E5%AF%BC%E5%87%BA.cpt%22%2C%22xn%22%3A%22""" + year + """%22%2C\
-%22xq%22%3A%22""" + str(smcount) + "%22%2C%22dm%22%3A%22" + UID + "%22%7D%5D"
+    def switch_proxies(self, func):
+        for p in self.proxy_ports:
+            try:
+                self.config_proxies(p)
+                func()
+            except Exception as error:
+                logging.debug(error)
+            else:
+                logging.info(f"代理端口设定为：{p}")
+                break
+        else:
+            raise ReportException.LoginError("无可用代理。")
 
-    response = sess.post(config.URLs.excel_export, params=excel_params,proxies=proxies1)
-    if response.status_code != 200:
-        raise CrawlerError("Get Excel: Server responded error code" + response.status_code + ".")
-    elif response.headers["content-type"].find("excel")==-1 and\
-         response.headers["content-type"].find("xls")==-1:
-        raise CrawlerError("Get Excel: Server not responding excel format.")
+    # log in jw system
+    def login(self, loginFormPage, allLoginParams):
 
-    return response.content
+        self.start_new_session()
+        # Add the given username and password into login params.
+        allLoginParams["username"] = config.CrawlerParams.username
+        allLoginParams["password"] = config.CrawlerParams.password
+
+        # Login!
+        # Get a validated COOKIE for our session.
+        response = self.session.post(config.URLs.login_domain + loginFormPage,
+                                     params=allLoginParams,
+                                     proxies=self.proxies)
+
+        if response.status_code != 200:
+            raise CrawlerError("Login: Server responded error code: " +
+                               str(response.status_code) + ".")
+        elif response.text.find("账号密码验证失败") != -1:
+            raise CrawlerError("Login: Incorrect username or password.")
+
+    def getExcelRawData(self) -> bytes:
+        response = self.session.post(config.URLs.uid_query,
+                                     proxies=self.proxies)
+        if response.status_code != 200:
+            raise CrawlerError("Query User UID: Server responded error code" +
+                               str(response.status_code) + ".")
+        try:
+            j = json.loads(response.text)
+            UID = j["ID"]
+        except json.JSONDecodeError:
+            raise CrawlerError(
+                "Query User UID: Get userinfo json from session failed.")
+        except KeyError:
+            raise CrawlerError(
+                "Query User UID: Cannot find id in requested userinfo json.")
+
+        excel_params = {"format": "excel", "_filename_": "export"}
+
+        year, smcount = misc.semester(config.DateTime.startDate)
+        # param "reportlets" seems to be a json-like stuff.
+        # inelegant, indeed. But I'm too lazy to make a change. ;)
+        excel_params[
+            "reportlets"] = """%5B%7B%22reportlet%22%3A%22%2Fbyyt%2Fpkgl%2F%E5%AD%A6%E7%94%9F%\
+    E4%B8%BB%E9%A1%B5%E8%AF%BE%E8%A1%A8%E5%AF%BC%E5%87%BA.cpt%22%2C%22xn%22%3A%22""" + year + """%22%2C\
+    %22xq%22%3A%22""" + str(
+                smcount) + "%22%2C%22dm%22%3A%22" + UID + "%22%7D%5D"
+
+        response = self.session.post(config.URLs.excel_export,
+                                     params=excel_params,
+                                     proxies=self.proxies)
+        if response.status_code != 200:
+            raise CrawlerError("Get Excel: Server responded error code" +
+                               response.status_code + ".")
+        elif response.headers["content-type"].find("excel")==-1 and\
+             response.headers["content-type"].find("xls")==-1:
+            raise CrawlerError(
+                "Get Excel: Server not responding excel format.")
+
+        return response.content
+
+    def get_text(self) -> typing.Tuple[str, dict]:
+        response = self.session.get(config.URLs.login_page,
+                                    proxies=self.proxies)
+        if response.status_code != 200:
+            raise CrawlerError("Get Login Page: Server responded error code" +
+                               str(response.status_code) + ".")
+
+        pageSoup = bs4.BeautifulSoup(response.text, "html.parser")
+        formSoup = pageSoup.find("form")
+
+        # Get form's attribute "action", which means action page
+        form_page = formSoup.get("action")
+
+        # Get all form input key and default values
+        default_values = dict()
+        inputSoups = formSoup.find_all("input")
+        for i in inputSoups:
+            default_values[i.get("name")] = i.get("value")
+
+        return form_page, default_values
--- main.py
+++ main.py
@@ -5,20 +5,42 @@ from excelParser import ProcessExcel
 from crawler import excelCrawler
 
 import click
+import random
+import logging
+import time
 import misc
 import datetime
-import pysocks
 # import genIcs
 
 
 def main():
-    form_url, form_inputs = excelCrawler.get_text()
+
+    def wait_a_minute(prompt, extra_minutes=0):
+        wait = 60 * extra_minutes + random.randint(0, 60)
+        logging.warning(prompt.format(wait))
+        time.sleep(wait)
+    r = excelCrawler.Report(proxy_on=1, ports=[1080, 2080])
+    form_url, form_inputs = r.get_text()
+    form_url, form_inputs = r.login(form_url, form_inputs)
     excelCrawler.login(form_url, form_inputs)
     excelRawBytes = excelCrawler.getExcelRawData()
 
     cal_name, cal_data = ProcessExcel.process(excelRawBytes)
     cal_name = "你的课表"
     icalOutput.output(cal_name, cal_data)
+
+    try:
+        r.student_login()
+    except excelCrawler.ReportException.LoginError:
+        wait_a_minute("登录失败，将在 {} 秒后重试。", 1)
+        r.login()
+    except Exception as err:
+        if r.proxy_on:
+            logging.error(err)
+            wait_a_minute("开启代理，将在 {} 秒后重试。")
+            r.switch_proxies(r.student_login)
+        else:
+            raise err
 
 
 @click.command()
